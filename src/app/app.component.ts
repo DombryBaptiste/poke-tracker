7685fb7c58261be84851c7df3a25219b78dd67c5
Baptiste <baptiste.dombry12@gmail.com>
2025-06-04 14:50:30 +0200

    V1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AllBoxComponent } from "./components/all-box/all-box.component";
+import { PokemonService } from './services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppData } from './entities/appData';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AllBoxComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'poke-tracker';
+  data: AppData;
+
+  constructor(public pokeService: PokemonService) {
+    this.data = this.pokeService.data();
+  }
+
+  
+onGenerationChange(event: Event): void {
+  const selectElement = event.target as HTMLSelectElement;
+  const selectedValue = +selectElement.value;
+  this.pokeService.setSelectedGeneration(selectedValue);
+}
 }
---
+++ src/app/entities/pokemonSelected.ts
@@ -0,0 +1,10 @@
+export interface PokemonSelected {
+    id: number;
+    color: PokemonColor
+}
+
+export enum PokemonColor {
+    Red,
+    Blue,
+    Black,
+}
