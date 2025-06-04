bb589ad983767aed0860087e3380e572086ff66a
Baptiste <baptiste.dombry12@gmail.com>
2025-06-04 22:23:41 +0200

    feature/Save + Import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AllBoxComponent } from "./components/all-box/all-box.component";
 import { PokemonService } from './services/pokemon.service';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,9 @@ import { AppData } from './entities/appData';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -16,20 +19,27 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     MatSelectModule,
     MatFormFieldModule,
-    ReactiveFormsModule, MatInputModule],
+    ReactiveFormsModule, MatInputModule, MatMenuModule, MatButtonModule, MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   data: AppData;
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
   constructor(public pokeService: PokemonService) {
     this.data = this.pokeService.data();
   }
 
   
-onGenerationChange(event: MatSelectChange): void {
-  const genNumber = event.value;
-  this.pokeService.setSelectedGeneration(genNumber);
+  onGenerationChange(event: MatSelectChange): void {
+    const genNumber = event.value;
+    this.pokeService.setSelectedGeneration(genNumber);
+  }
+
+  triggerFileInput(): void {
+  this.fileInput.nativeElement.click(); // Déclenche l'ouverture du sélecteur de fichier
 }
 }
